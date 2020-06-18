82dbff09792d77086b8a6e707bc95c354c2adf3d
liushuyu <liushuyu011@gmail.com>
2020-06-17 23:55:22 -0600

    bump version to 20200617

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="acbs",
-    version="0.1.20200615",
+    version="0.1.20200617",
     author="liushuyu",
     author_email="liushuyu@aosc.io",
     description="AOSC CI Building System",
