1a5b4560e294fbd0fc457fcd1e49824f3dd2d978
Cyan <cyanoxygen@aosc.io>
2022-10-31 21:50:20 +0800

    ab3cfg: improve file reading and parsing logic
    - Replace try...catch block in file reading with 'with open()' statement
    
    Co-Authored-By: liushuyu <liushuyu011@gmail.com>

--- acbs/ab3cfg.py
+++ acbs/ab3cfg.py
@@ -11,27 +11,24 @@ class AB3Cfg(object):
         :param path: path to the ab3cfg.sh file, with the filename
         '''
         self.cfgpath = path
-        self.file = None
         self.vars = None
         self.stage2 = False
         try:
-            self.file = open(self.cfgpath, 'rt')
-        except IOError:
+            with open(self.cfgpath, 'rt') as cfgfile:
+                self.parse(cfgfile)
+        except Exception as e:
             # You run ACBS then you should have AB3, eh?
             raise RuntimeError(f'Autobuild3 config file {self.cfgpath} does not exist\n' +
-                               'Unable to read Autobuild3 config file.')
-        if self.file:
-            self.parse()
+                               'Unable to read Autobuild3 config file.') from e
 
-    def parse(self):
+    def parse(self, file):
         '''
         Parse the ab3cfg.sh file.
         '''
-        if self.file:
-            try:
-                self.vars = bashvar.eval_bashvar(self.file.read(), filename=self.cfgpath)
-            except Exception as e:
-                raise RuntimeError(f'Error parsing autobuild3 config file: {e}.')
+        try:
+            self.vars = bashvar.eval_bashvar(file.read(), filename=self.cfgpath)
+        except Exception as e:
+            raise RuntimeError(f'Error parsing autobuild3 config file: {e}.') from e
 
     def is_in_stage2(self) -> bool:
         '''
