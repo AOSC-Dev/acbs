16f7aa513cf70da03648022ed04e9495dbb096c1
liushuyu <liushuyu011@gmail.com>
2025-07-07 10:48:12 +0800

    base: use dataclass to represent base data containers

--- acbs/base.py
+++ acbs/base.py
@@ -1,70 +1,62 @@
 import re
+from dataclasses import dataclass, field
 from typing import Dict, List, Optional, Tuple
 
 from acbs import __version__
 
 
-class ACBSSourceInfo(object):
-    def __init__(self, type: str, url: str, revision=None, branch=None, depth=None) -> None:
-        self.type = type
-        self.url = url
-        self.revision: Optional[str] = revision
-        self.branch: Optional[str] = branch
-        self.depth: Optional[int] = depth
-        self.chksum: Tuple[str, str] = ('', '')
-        self.source_name: Optional[str] = ''
-        self.use_url_name: bool = False
-        # where the source file/folder is located (on local filesystem)
-        self.source_location: Optional[str] = None
-        self.enabled: bool = True
-        # copy the repository to the build directory
-        self.copy_repo: bool = False
-        # this is a tristate: 0 - off; 1 - on (non-recursive); 2 - recursive
-        self.submodule: int = 2
-
-    def __repr__(self) -> str:
-        return '<ACBSSourceInfo {type}: {url}:{branch}@{revision} integrity: {checksum}>'.format(type=self.type, url=self.url, branch=self.branch, revision=self.revision, checksum=self.chksum)
+@dataclass
+class ACBSSourceInfo:
+    type: str
+    url: str
+    revision: Optional[str] = None
+    branch: Optional[str] = None
+    depth: Optional[int] = None
+    chksum: Tuple[str, str] = ('', '')
+    source_name: Optional[str] = ''
+    use_url_name: bool = False
+    # where the source file/folder is located (on local filesystem)
+    source_location: Optional[str] = None
+    enabled: bool = True
+    # copy the repository to the build directory
+    copy_repo: bool = False
+    # this is a tristate: 0 - off; 1 - on (non-recursive); 2 - recursive
+    submodule: int = 2
 
 
-class ACBSPackageInfo(object):
-    def __init__(self, name: str, deps: List[str], location: str, source_uri: List[ACBSSourceInfo]) -> None:
-        self.name = name
-        self.rel = '0'
-        self.deps = deps
-        self.installables: List[str] = []  # installable dependencies
-        self.build_location = ''
-        self.base_slug = ''  # group slug (like extra-devel/llvm), if any
-        self.group_seq = 0  # group sequence number
-        self.source_uri = source_uri
-        self.version = ''
-        self.epoch = ''
-        self.subdir: Optional[str] = None
-        # fail_arch regex
-        self.fail_arch: Optional[re.Pattern] = None
-        self.bin_arch = ''
-        # script location (autobuild directory)
-        self.script_location = location
-        # extra exported variables from spec
-        self.exported: Dict[str, str] = {}
-        # modifiers to be applied to the source file/folder (only available in autobuild4)
-        self.modifiers: str = ''
+@dataclass
+class ACBSPackageInfo:
+    name: str
+    deps: List[str]
+    location: str
+    source_uri: List[ACBSSourceInfo]
+    rel: str = '0'
+    installables: List[str] = field(default_factory=list)
+    build_location: str = ''
+    base_slug: str = ''  # group slug (like extra-devel/llvm), if any
+    group_seq: int = 0  # group sequence number
+    version: str = ''
+    epoch: str = ''
+    subdir: Optional[str] = None
+    fail_arch: Optional[re.Pattern] = None  # fail_arch regex
+    bin_arch: str = ''
+    script_location: str = ''  # script location (autobuild directory)
+    exported: Dict[str, str] = field(default_factory=dict)  # extra exported variables from spec
+    modifiers: str = ''  # modifiers to be applied to the source file/folder (only available in autobuild4)
+
 
     @staticmethod
     def is_in_stage2(modifiers: str) -> bool:
         return '+stage2' in modifiers.lower()
 
 
-    def __repr__(self) -> str:
-        return f'<ACBSPackageInfo {self.name}: deps: {self.deps} ; uri: {self.source_uri}>'
-
-
-class ACBSShrinkWrap(object):
-    def __init__(self, cursor: int, timings: List[Tuple[str, float]], packages: List[ACBSPackageInfo], no_deps: bool):
-        self.cursor = cursor
-        self.timings = timings
-        self.packages = packages
-        # spec states
-        self.sps: List[str] = []
-        self.dpkg_state: str = ''
-        self.no_deps = no_deps
-        self.version = __version__
+@dataclass
+class ACBSShrinkWrap:
+    cursor: int
+    timings: List[Tuple[str, float]]
+    packages: List[ACBSPackageInfo]
+    no_deps: bool
+    # spec states
+    sps: List[str] = field(default_factory=list)
+    dpkg_state: str = ''
+    version: str = __version__
